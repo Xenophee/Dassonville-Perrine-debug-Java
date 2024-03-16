35088bfbe873ffa6552569d4267f57bad603751a
Xenophee <perrine.dassonville@outlook.fr>
2024-03-16 20:01:01 +0100

    refactor analytics counter

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,45 +1,44 @@
 package com.hemebiotech.analytics;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.util.HashMap;
+import java.io.IOException;
+import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class AnalyticsCounter {
-	private static int headacheCount = 0;
-	private static int rashCount = 0;
-	private static int pupilCount = 0;
-	
-	public static void main(String[] args) throws Exception {
-		// first get input
-		String fileName = "Project02Eclipse/symptoms.txt";
-		BufferedReader reader = new BufferedReader (new FileReader(fileName));
-		String line = reader.readLine();
 
+	private final ISymptomReader reader;
+	private final ISymptomWriter writer;
 
-		while (line != null) {
-
-			if (line.equals("headache")) {
-				headacheCount++;
-			}
-			else if (line.equals("rash")) {
-				rashCount++;
-			}
-			else if (line.contains("pupils")) {
-				pupilCount++;
-			}
-
-			line = reader.readLine();
-		}
-
-
-		Map<String, Integer> symptoms = new HashMap<>();
-		symptoms.put("headache", headacheCount);
-		symptoms.put("rash", rashCount);
-		symptoms.put("dialated pupils", pupilCount);
-
-		WriteSymptomDataToFile resultFile = new WriteSymptomDataToFile("result.out");
-		resultFile.writeSymptoms(symptoms);
-
+	public AnalyticsCounter(ISymptomReader reader, ISymptomWriter writer) {
+		this.reader = reader;
+		this.writer = writer;
 	}
+
+	public List<String> getSymptoms() {
+		return reader.GetSymptoms();
+	}
+
+
+	public Map<String, Integer> countSymptoms(List<String> symptoms) {
+        Map<String, Integer> symptomCount = symptoms.stream()
+				.collect(Collectors.groupingBy(symptom -> symptom, Collectors.summingInt(e -> 1)));
+
+        return symptomCount;
+	}
+
+	public Map<String, Integer> sortSymptoms(Map<String, Integer> symptoms) {
+		Map<String, Integer> sortedSymptoms = symptoms.entrySet().stream()
+				.sorted(Map.Entry.comparingByKey())
+				.collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (oldValue, newValue) -> oldValue, LinkedHashMap::new));
+
+		return sortedSymptoms;
+	}
+
+
+	public void writeSymptoms(Map<String, Integer>symptoms) throws IOException {
+		writer.writeSymptoms(symptoms);
+	}
+
 }
