ee11c2e986111e1f0d10579d44c69543aa3a3ffb
Xenophee <perrine.dassonville@outlook.fr>
2024-03-19 17:33:01 +0100

    restructuring

--- Project02Eclipse/src/com/hemebiotech/analytics/ISymptomWriter.java
+++
@@ -1,9 +0,0 @@
-package com.hemebiotech.analytics;
-
-import java.io.IOException;
-import java.util.Map;
-
-public interface ISymptomWriter {
-
-    void writeSymptoms(Map<String, Integer> symptoms) throws IOException;
-}
